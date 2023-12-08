db6749a12f74118f94f7c43b2f31687239ddf42a
IResetic <iresetic.skybit@gmail.com>
2023-12-09 00:47:54 +0100

    Add hilt di to the project

---
+++ app/src/main/java/com/example/githubuserfilter/main/GithubFilterApplication.kt
@@ -0,0 +1,7 @@
+package com.example.githubuserfilter.main
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GithubFilterApplication : Application()
--- app/src/main/java/com/example/githubuserfilter/main/MainActivity.kt
+++ app/src/main/java/com/example/githubuserfilter/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.githubuserfilter
+package com.example.githubuserfilter.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.githubuserfilter.ui.theme.GithubUserFilterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
